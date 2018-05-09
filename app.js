39db1f023eb73f491ab492944968fc6832366327
Edward Xia <xias1@uci.edu>
2018-05-09 16:55:40 -0700

    Remove custom domain

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Credentials', 'true')
   res.header('Access-Control-Allow-Headers', 'Content-Type')
   res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, PATCH, DELETE')
-  res.header('Access-Control-Allow-Origin', req.app.get('env') === 'development' ? 'http://localhost:8080' : 'https://zot.academy')
+  res.header('Access-Control-Allow-Origin', req.app.get('env') === 'development' ? 'http://localhost:8080' : 'https://zotacademy.github.io')
   next()
 })
 
